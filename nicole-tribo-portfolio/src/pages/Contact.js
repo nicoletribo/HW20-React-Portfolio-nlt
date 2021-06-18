ee75ca46e19a35695dcb26eff9b4795a1a70eefd
Nicole Tribo <Nicoletribo@yahoo.com>
2021-06-18 14:49:43 -0500

    renders the contact form

---
+++ nicole-tribo-portfolio/src/pages/Contact.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ContactForm from "../components/ContactForm";
+
+
+class Contact extends Component {
+  
+  render() {
+    return (
+      <div>
+        
+          <ContactForm
+          />
+          
+      </div>
+    );
+  }
+}
+
+export default Contact;
