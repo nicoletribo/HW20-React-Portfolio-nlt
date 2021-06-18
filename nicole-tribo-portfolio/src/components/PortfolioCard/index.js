f9561def84dcc3862b334571a44e3787366f6a25
Nicole Tribo <Nicoletribo@yahoo.com>
2021-06-18 14:48:10 -0500

    project card looks good

---
+++ nicole-tribo-portfolio/src/components/PortfolioCard/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+function PortfolioCard(props) {
+  return (
+      <div className="columns features">
+          <div className="column is-4">
+            <div className="card is-flexible">
+              <div className="card-image">
+                <figure className="image is-2by2">
+                  <img alt={props.name} src={props.image} />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="content has-text-centered">
+                  <h4>{props.name}</h4>
+                  <p>{props.description}</p>
+                  <div className="button is-$blue: hsl(229, 53%, 53%)">
+                    <a href={props.view}>Application</a>
+                  </div>
+                  <div className="button">
+                    <a href={props.repository}>GitHub Repository</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  );
+}
+
+export default PortfolioCard;
