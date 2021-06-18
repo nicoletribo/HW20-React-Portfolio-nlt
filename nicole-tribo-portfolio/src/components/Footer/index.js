f66a2798a2c8dc2a60c08ac286e1a92d73e5d85e
Nicole Tribo <Nicoletribo@yahoo.com>
2021-06-18 14:47:35 -0500

    footer looks good

---
+++ nicole-tribo-portfolio/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>This website was made by Nicole Tribo</strong>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
